03400a43a7db34eb15fa7f3e72e9bd4ed26fb4d9
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2020-09-28 15:02:00 +0100

    Update start_worker.sh

--- bash_scripts/start_worker.sh
+++ bash_scripts/start_worker.sh
@@ -10,13 +10,13 @@ echo "I am at " $HOSTNAME "and the master is " $hostmaster
 
 if [ $HOSTNAME != $hostmaster ] && [ $HOSTNAME != $hostdriver ]
 then
- echo "Started SLAVE on `hostname`"
+ echo "Started a WORKER on `hostname`"
  echo $HOSTNAME >> worker.log
  
  cd $SPARK_HOME/
  sbin/start-slave.sh $hostmaster:7077
 
 else
-  echo "Master" $hostmaster "or driver node" $hostdriver" - I dont start an SLAVE on " $HOSTNAME
+  echo "Master" $hostmaster "or driver node" $hostdriver" - I dont start a WORKER on " $HOSTNAME
 fi
 
